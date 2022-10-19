5701648c033434f38cf407ce3bbe6f437bbd6d38
BaoDu <devtranbaodu1602@gmail.com>
2022-10-19 18:32:00 +0700

    delete

--- Cau1/src/main/java/dao/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-
-}
--- Cau1/src/main/java/ktpm/cau1/model/OrderDto.java
+++
@@ -1,9 +0,0 @@
-package ktpm.cau1.model;
-
-import lombok.Data;
-
-@Data
-public class OrderDto {
-	private String name;
-	private double price;
-}
