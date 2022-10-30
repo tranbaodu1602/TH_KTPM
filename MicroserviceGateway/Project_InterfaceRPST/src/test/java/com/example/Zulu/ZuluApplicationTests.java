1dd673b70af2ceea83ec2f175bebc41f0806b344
BaoDu <devtranbaodu1602@gmail.com>
2022-10-30 19:00:56 +0700

    Tuan8

---
+++ MicroserviceGateway/Project_InterfaceRPST/src/test/java/com/example/Zulu/ZuluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
